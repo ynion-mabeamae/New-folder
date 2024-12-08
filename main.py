7b1c87792e4f67403623da7a0217d5e8625f6a7a
ynion-mabeamae <mabeamaeynion@gmail.com>
2024-12-09 00:53:19 +0800

    Add common base.

---
+++ main.py
@@ -0,0 +1,29 @@
+from bnpackages import ynion
+
+def display_main_menu():
+    while True:
+        print("1. Delima")
+        print("2. Ynion")
+        print("3. Relente")
+        print("4. Quiambao")
+        print("5. Citron")
+        print("6. Exit")
+        
+        choice = int(input("Enter your choice: "))
+
+        match choice:
+            case 1:
+                pass
+            case 2:
+                ynion.display_menu()
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                pass
+            case 6:
+                print("\nExiting the menu.")
+                return
+
+display_main_menu()
